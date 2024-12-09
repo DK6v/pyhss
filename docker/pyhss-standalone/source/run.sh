1bf8557f47da7bf331a88807625182967f027d21
Korobkov Dmitry <korobkov.dmitry@huawei-partners.com>
2024-12-09 13:21:37 +0300

    Docker: update logging settings

--- docker/pyhss-standalone/source/run.sh
+++ docker/pyhss-standalone/source/run.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
-ls -al /pyhss
-ls -al /pyhss/bin
-
 cd ./services || exit 1
 
-echo " * Start API service"
+echo -n " * Starting API service... "
 python3 apiService.py --host="${PYHSS_COMPONENT_NAME}" --port=8080 &
+api_service_pid=$!
+echo "Done (pid: ${api_service_pid})"
+
 # Sleep is needed to let db be populated in a non-overlapping fashion
 sleep 5
 
-echo " * Start Diameter service"
+echo -n " * Starting Diameter service... "
 python3 diameterService.py &
-# Sleep is needed to let db be populated in a non-overlapping fashion
-sleep 5
+diameter_service_pid=$!
+echo "Done (pid: ${diameter_service_pid=$!})"
 
-if [ "$PYHSS_LOG_ENABLED" == "true" ]; then
-    echo " * Start Log service"
+if [ "$PYHSS_LOG_FILES_ENABLED" == "true" ]; then
+    echo -n " * Starting Log service... "
     python3 logService.py &
+    log_service_pid=$!
+    echo "Done (pid: ${log_service_pid=$!})"
 fi
 
-echo " * Start HSS service"
-python3 hssService.py
+echo -n " * Starting HSS service... "
+python3 hssService.py &
+hss_service_pid=$!
+echo "Done (pid: ${hss_service_pid=$!})"
+
+wait ${hss_service_pid} 2>/dev/null
